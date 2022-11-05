fa69bf3b6eb700b5b711eead3eab0b73ec3b1a79
221Roberto221 <andrade.roberto@uabc.edu.mx>
2022-11-04 18:00:31 -0700

    Agregar archivos de proyecto.

---
+++ LotusV3/LotusV3/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using LotusV3.Tables;
+using LotusV3.Views;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LotusV3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void IniciarSesion_Clicked(object sender, EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDataBase.db");
+            var db = new SQLiteConnection(dbpath);
+            var myquery = db.Table<RegUserTable>().Where(u => u.UserName.Equals(EntryUser.Text) && u.Password.Equals(EntryPassword.Text)).FirstOrDefault();
+
+            if (myquery != null)
+            {
+                App.Current.MainPage = new NavigationPage(new HomePage());
+
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    var result = await this.DisplayAlert("Error", "Failed User Name and Password", "Yes", "Cancel");
+                    if (result)
+                        await Navigation.PushAsync(new MainPage());
+                    else
+                    {
+                        await Navigation.PushAsync(new MainPage());
+                    }
+                });
+            }
+        }
+
+        private async void NuevoUsuario_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+    }
+}
---
+++ LotusV3/LotusV3/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using LotusV3.Tables;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LotusV3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Registrar_Clicked(object sender, EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDataBase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text,
+            };
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulation", "User Registeration Sucessfull", "Yes", "Cancel");
+                if (result)
+                    await Navigation.PushAsync(new MainPage());
+            });
+
+        }
+    }
+}
